1bb5aa4761f9775d8590668115d9cf14a0148100
dunitian <1054186320@qq.com>
2015-12-10 12:14:16 +0800

    逆天WP-Win10开发笔记源码（学习ing）

---
+++ 02.XAMLBaseDKill/02.XAMLBaseDKill.Shared/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.XAMLBaseDKill
+{
+    public  class Person
+    {
+        /// <summary>
+        /// 编号
+        /// </summary>
+        private int _id;
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        /// <summary>
+        /// 年龄
+        /// </summary>
+        private int _age;
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        /// <summary>
+        /// 工资
+        /// </summary>
+        private double _salary;
+        public double Salary
+        {
+            get { return _salary; }
+            set { _salary = value; }
+        }
+       
+    }
+}
